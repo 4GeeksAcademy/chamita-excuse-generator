ada744dac2d302faa1a33af918cd5f88c39c8e4b
Gen <geniabichino@gmail.com>
2025-03-08 22:15:01 +0000

    a

--- src/app.js
+++ src/app.js
@@ -1,11 +1,64 @@
-import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+
+  let who = ['The dog', 'My daugther', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed','doodled', 'crushed', 'broke'];
+  let what = ['my homework','my face', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping','when I was taking a shower', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let chunks = [who, action, what, when]
+
+  let result = ''
+
+  for (let item  of chunks) {
+    let newString = item [(Math.floor(Math.random() * item.length))]
+    result = result + ' ' + newString
+  }
+
+  let excusaElement = document.getElementById("excusa")
+  excusaElement.innerHTML = (result)
+  
+}
+
+  //Second solution.
+
+
+//   let randomwho = (Math.floor(Math.random() * who.length))
+//   let randomaction = (Math.floor(Math.random() * action.length))
+//   let randomwhat = (Math.floor(Math.random() * what.length))
+//   let randomwhen = (Math.floor(Math.random() * when.length))
+
+
+//   let excuse = `${who[randomwho]} ${action[randomaction]} ${what[randomwhat]} ${when[randomwhen]}`
+//   let excusaElement = document.getElementById("excusa")
+//   excusaElement.innerHTML = (excuse)
+
+
+
+//background color
+
+const getRandomNumber = (maxNum) => {
+  return Math.floor(Math.random() * maxNum);
 };
+
+const getRandomColor = () => {
+  const h = getRandomNumber(360);
+  const s = getRandomNumber(30);
+  const l = getRandomNumber(70);
+  return `hsl(${h}deg, ${s}%, ${l}%)`;
+};
+
+const setBackgroundColor = () => {
+  const randomColor = getRandomColor();
+  document.getElementById("background").style.backgroundColor = randomColor;
+}
+
+setBackgroundColor()
+
+
+
+ 
